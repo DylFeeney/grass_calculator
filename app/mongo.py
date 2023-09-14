4bc54eec956105c37b18d4ef381b38b714e9841c
Dylan Feeney <itsdylanfeeney@gmail.com>
2023-09-15 00:18:14 +0100

    Added in logic to set the first round number, retrieve the round number and update the round number

--- app/mongo.py
+++ app/mongo.py
@@ -1,3 +1,4 @@
+import pymongo.errors
 from pymongo import MongoClient
 import json
 from bson.json_util import dumps, loads
@@ -10,18 +11,19 @@ class Mongo:
     def __init__(self):
         self.client = MongoClient('database', 27017, username="root", password="rootpassword")
 
-    def retrieve_collection(self, database, collection):
-        mydb = self.client[database]
-        mycol = mydb[collection]
-        return mycol
+    def retrieve_collection(self, database_name, collection_name):
+        database = self.client[database_name]
+        collection = database[collection_name]
+        return database, collection
 
-    def insert_player_name(self, database, collection, player_name):
-        collection = self.retrieve_collection(database, collection)
+    # Player Name
+    def insert_player_name(self, database_name, collection_name, player_name):
+        database, collection = self.retrieve_collection(database_name, collection_name)
         mydict = {"name": player_name}
         collection.insert_one(mydict)
 
-    def list_players(self, database, collection):
-        collection = self.retrieve_collection(database, collection)
+    def list_players(self, database_name, collection_name):
+        database, collection = self.retrieve_collection(database_name, collection_name)
         response = collection.find({}, {"name": 1, "_id": 0})
 
         player_names = []
@@ -29,3 +31,27 @@ class Mongo:
             player_names.append(doc["name"])
 
         return player_names
+
+    def retrieve_round_number(self, database_name):
+        round_numbers = "round_numbers"
+        database, collection = self.retrieve_collection(database_name, round_numbers)
+        try:
+            database.validate_collection(round_numbers)
+        except pymongo.errors.OperationFailure:  # Collection does not exist yet
+            return self.insert_first_round(collection)
+
+        response = collection.find_one({}, {"round_number": 1, "_id": 0})
+        return response["round_number"]
+
+
+    def insert_first_round(self, collection):
+        data_to_insert = {"round_number": 1}
+        collection.insert_one(data_to_insert)
+        return 1
+
+    def update_round_number(self, database_name, current_round_number):
+        new_round_number = current_round_number + 1
+        database, collection = self.retrieve_collection(database_name, "round_numbers")
+        old_data = {"round_number": current_round_number}
+        new_data = {"$set": {"round_number": new_round_number}}
+        collection.update_one(old_data, new_data)
